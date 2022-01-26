9dc5800c1f4c6dec6ecc309e53a11d0d603510d9
alessandrojuza <mascia.alessandro89@gmail.com>
2022-01-26 17:09:15 +0100

    Added Shortener component and basic HTML structure

---
+++ src/components/Shortener.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Shortener.scss";
+
+const Shortener = () => {
+  return (
+    <div className="shortener">
+      <input type="text" />
+      <button>SHORTEN</button>
+    </div>
+  );
+};
+
+export default Shortener;
