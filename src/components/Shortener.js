445ca57855371d738388c72db3f4822ece0cc0b2
alessandrojuza <mascia.alessandro89@gmail.com>
2022-01-29 16:56:56 +0100

    Completed first page layout

--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 import "../styles/Shortener.scss";
 import axios from "axios";
-import mainImage from "../img/main-image.svg";
+import mainImage from "../img/main-image.png";
 
 const Shortener = () => {
   const getShortenedUrl = () => {
@@ -17,10 +17,18 @@ const Shortener = () => {
   return (
     <div className="shortener">
       <div className="text-container">
+        <h1>
+          Your <span className="text-urls">URLs</span>, made easy.
+        </h1>
+        <p className="description-text">
+          2Short allows you to share all of your links, So you don't need to
+          change it before past posting on Facebook, or tweet your links, You
+          only need to remember the easy URL!
+        </p>
         <input type="text" onChange={(e) => setOriginalUrl(e.target.value)} />
         <button onClick={getShortenedUrl}>SHORTEN</button>
-        <h1>My Url: {shortenedUrl}</h1>
-        <h1>Input: {originalUrl}</h1>
+        {/* <h1>My Url: {shortenedUrl}</h1>
+        <h1>Input: {originalUrl}</h1> */}
       </div>
       <img src={mainImage} alt="main image" className="main-image" />
     </div>
